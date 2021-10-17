b2ad4f9af644c68a9d94b2ecd43ec9c602088be9
Sarah Baghdadi <sera.baghdadi@gmail.com>
2021-10-16 22:54:35 -0400

    Display books on home page

--- app.js
+++ app.js
@@ -9,9 +9,27 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Require sequelize
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'library.db'
+});
+
+// async IIFE
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database successful!');
+    await sequelize.sync({ force: true });
+  } catch (error) {
+
+  }
+})();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll({ order: [["createdAt", "DESC"]] });
+  res.render('index', { books, title: 'Books' });
+}));
+
+/* GET articles listing. */
+//router.get('/', asyncHandler(async (req, res) => {
+  //const books = await Book.findAll({ order: [["createdAt", "DESC"]] });
+  //res.render("articles/index", { articles, title: "Sequelize-It!" });
+//}));
 
 module.exports = router;
